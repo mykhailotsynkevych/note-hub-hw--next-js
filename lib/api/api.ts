9dfdd04ef027c4c510526d723496b2bdac787064
Mykhailo Tsynkevych <tsynkevych@gmx.de>
2026-09-16 14:19:10 +0200

    fix: use NEXT_PUBLIC_BASE_URL for axios baseURL

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = process.env.NEXT_PUBLIC_NOTES_URL;
 export const nextServer = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api`,
   withCredentials: true, // дозволяє axios працювати з cookie
 });
